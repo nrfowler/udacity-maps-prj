957fa813c514f325c90ddf944d418d8d93bbb292
nrfowler <nrfowler@gmail.com>
2017-12-11 16:05:07 -0600

    refactor: ajax calls in a for loop
    Fill the list with ajax calls to foursquare; bind the VM at the end of the last ajax call.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 var map
-
 var toggleActive = function (place){
   // if active, close infowindow and animation
   if(place.isActive){
@@ -11,26 +10,21 @@ var toggleActive = function (place){
   else
     infoWindow.close()
   place.isActive = true
-
   let marker = place.marker
   // if (marker.getAnimation() !== null) {
   //   marker.setAnimation(null)
   // } else {
   //   marker.setAnimation(google.maps.Animation.BOUNCE)
   // }
-
-            marker.isActive = true
-            contentString = '<h1>'+(place.name || "") +'</h1>'
-            +'<h3>'+ (place.description || "")+'</h3>'
-                            +'<div> Phone: '+(place.phone|| "N/A") +'</div>'
-+'<div> Address: '+(place.address || "N/A") +'</div>'
- infoWindow = new google.maps.InfoWindow({
+  marker.isActive = true
+  contentString = '<h1>'+(place.name || "") +'</h1>'
+  +'<h3>'+ (place.description || "")+'</h3>'
+  +'<div> Phone: '+(place.phone|| "N/A") +'</div>'
+  +'<div> Address: '+(place.address || "N/A") +'</div>'
+  infoWindow = new google.maps.InfoWindow({
             content: contentString})
-            infoWindow.open(map, marker)
-
-
+  infoWindow.open(map, marker)
 }
-
 var infoWindow
 var initMap = function () {
   console.log("init map")
@@ -40,55 +34,95 @@ map = new google.maps.Map(document.getElementById('map'), {
 center: {"lat": 41.58989350941445,
         "lng": -93.61109381248022},
 zoom: 13})
-var placeItemModel = function(marker,name,description, phone, address, venueId, isActive){
-  this.marker = ko.observable(marker)
+var placeItemModel = function(marker,name,description, phone,
+  address, venueId, isActive, isVisible){
+  this.marker = marker
   this.name = ko.observable(name)
   this.description = ko.observable(description)
   this.phone = ko.observable(phone)
   this.address = ko.observable(address)
   this.venueId = ko.observable(venueId)
   this.isActive = ko.observable(isActive)
+  this.isVisible = ko.observable(isVisible)
 }
-    var listVenueIds=[
-'524dc775498e350c4418dc7b'
-,
-'4b8f2be4f964a520244c33e3'
-,
-'4aa8662af964a520205120e3'
-,
-'4b46baddf964a5207c2726e3'
-,
+var listVenueIds=[
+'524dc775498e350c4418dc7b',
+'4b8f2be4f964a520244c33e3',
+'4aa8662af964a520205120e3',
+'4b46baddf964a5207c2726e3',
 '4b4658aff964a520371e26e3'
 ]
+var vm = new ViewModel()
+var i =0
 var listPlaces = []
     for (let id of listVenueIds) {
-
-      let urlString = 'https://api.foursquare.com/v2/venues/'+id+'?v=20171010&client_id='+
-      'SEHUEOSE3XRMJMKEK5SZVIQE3DKILVAKUJMAMQQAUWQSHWSY&client_secret=21PN43B0IGHQJNHUEHXEAAKA1VTBA5WPXEZD3MRKNXP0ZRRK'
+      let urlString = 'https://api.foursquare.com/v2/venues/'+id+
+      '?v=20171010&client_id='+
+      'SEHUEOSE3XRMJMKEK5SZVIQE3DKILVAKUJMAMQQAUWQSHWSY&client_'+
+      'secret=21PN43B0IGHQJNHUEHXEAAKA1VTBA5WPXEZD3MRKNXP0ZRRK'
       $.ajax({
         url: urlString,
         method: "GET",
       success: function(data){
-        var element = new placeItemModel({},'','','','',id, false)
+        var element = new placeItemModel({},'','','','',id, false, true)
         element.name = data.response.venue.name
         element.description = data.response.venue.description
         element.phone = data.response.venue.contact.formattedPhone
         element.address = data.response.venue.location.address
         element.marker = new google.maps.Marker({
-        position: {lat: data.response.venue.location.lat, lng: data.response.venue.location.lng},
+        position: {lat: data.response.venue.location.lat,
+          lng: data.response.venue.location.lng},
         map: map,
         animation: google.maps.Animation.DROP,
         title: data.response.venue.name})
         element.marker.addListener('click',function(){toggleActive(element)})
         listPlaces.push(element)
-
-
+        vm.listPlaces().push(element)
+        if(listVenueIds.length == i+1)
+          ko.applyBindings(vm)
+        i=i+1
       },
       error: function(data){
         console.log("error retrieving data ")
       }})
     }
-  console.log(listPlaces)
-  
-ko.applyBindings( new ViewModel(listPlaces))
  }
+var ViewModel = function () {
+  let self = this
+  self.listPlaces = ko.observableArray([])
+  function setMapOnAll(map) {
+        for (var i = 0; i < self.listPlaces().length; i++) {
+          self.listPlaces()[i].marker.setMap(map);
+        }
+      }
+  self.mySearch = function(){
+  console.log("my search "+document.getElementById("mySearch").value)
+  var query = document.getElementById("mySearch").value
+  //listPlaces match on title
+  //set isVisible = true
+    setMapOnAll(null)
+  console.log(self.listPlaces())
+  for (var place of self.listPlaces()) {
+    if(place.marker.title.toLowerCase().indexOf(query.toLowerCase()) !== -1){
+      place.isVisible(true)
+      place.marker.setMap(map)
+    }
+    else {
+      place.isVisible(false)
+    }
+  }
+}
+  self.myCancel = function(){
+        setMapOnAll(map)
+        for (var place of self.listPlaces()) {
+            place.isVisible(true)
+        }
+    }
+  self.onClick = function (place){
+    console.log("on click "+place.marker.title)
+    for (var placem of self.listPlaces()) {
+        placem.isActive=false
+    }
+      toggleActive(place)
+  }
+}
