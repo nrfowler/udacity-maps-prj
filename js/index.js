82cd300c9ee4ec4eef21eed14c670156d7599ea3
nrfowler <nrfowler@gmail.com>
2017-12-11 19:55:00 -0600

    refactor: Separate markers and knockout
    Separated markers into their own array but their is still some handling of markers in ko...

--- js/index.js
+++ js/index.js
@@ -1,22 +1,31 @@
 var map
-var toggleActive = function (place){
+
+var toggleActive = function (place,listMarkers){
   // if active, close infowindow and animation
-  if(place.isActive){
+  var findPlaceIdx = function(place){
+    for(let idx in listMarkers){
+      if(place.name==listMarkers[idx].title){
+        return idx
+      }
+    }
+    return 0
+  }
+  if(place.isActive()){
     infoWindow.close()
-    //marker.setAnimation(null)
-    place.isActive = false
+    console.log("already active, shutting it down")
+    place.isActive(false)
     return
   }
   else
     infoWindow.close()
-  place.isActive = true
-  let marker = place.marker
+  place.isActive(true)
+  let marker = listMarkers[findPlaceIdx(place)]
   // if (marker.getAnimation() !== null) {
   //   marker.setAnimation(null)
   // } else {
   //   marker.setAnimation(google.maps.Animation.BOUNCE)
   // }
-  marker.isActive = true
+  //marker.isActive = true
   contentString = '<h1>'+(place.name || "") +'</h1>'
   +'<h3>'+ (place.description || "")+'</h3>'
   +'<div> Phone: '+(place.phone|| "N/A") +'</div>'
@@ -27,16 +36,50 @@ var toggleActive = function (place){
 }
 var infoWindow
 var initMap = function () {
-  console.log("init map")
+  var ViewModel = function () {
+    let self = this
+    self.listPlaces = ko.observableArray([])
+    self.mySearch = function(){
+    console.log("my search "+document.getElementById("mySearch").value)
+    var query = document.getElementById("mySearch").value
+      setMapOnAll(null)
+    console.log(self.listPlaces())
+    for (var idx in self.listPlaces()) {
+      var place = self.listPlaces()[idx]
+      if(place.name.toLowerCase().indexOf(query.toLowerCase()) !== -1){
+        place.isVisible(true)
+        listMarkers[idx].setMap(map)
+      }
+      else {
+        listMarkers[idx].setMap(null)
+        place.isVisible(false)
+      }
+    }
+  }
+    self.myCancel = function(){
+          setMapOnAll(map)
+          for (var place of self.listPlaces()) {
+              place.isVisible(true)
+          }
+          document.getElementById("mySearch").value=""
+
+      }
+    self.onClick = function (place){
+      for (var placem of self.listPlaces()) {
+        if(placem!=place)
+          placem.isActive(false)
+      }
+        toggleActive(place,listMarkers)
+    }
+  }
    infoWindow = new google.maps.InfoWindow({
   content: null})
 map = new google.maps.Map(document.getElementById('map'), {
 center: {"lat": 41.58989350941445,
         "lng": -93.61109381248022},
 zoom: 13})
-var placeItemModel = function(marker,name,description, phone,
+var placeItemModel = function(name,description, phone,
   address, venueId, isActive, isVisible){
-  this.marker = marker
   this.name = ko.observable(name)
   this.description = ko.observable(description)
   this.phone = ko.observable(phone)
@@ -52,77 +95,45 @@ var listVenueIds=[
 '4b46baddf964a5207c2726e3',
 '4b4658aff964a520371e26e3'
 ]
+
+var listMarkers =[]
 var vm = new ViewModel()
-var i =0
 var listPlaces = []
-    for (let id of listVenueIds) {
-      let urlString = 'https://api.foursquare.com/v2/venues/'+id+
-      '?v=20171010&client_id='+
-      'SEHUEOSE3XRMJMKEK5SZVIQE3DKILVAKUJMAMQQAUWQSHWSY&client_'+
-      'secret=21PN43B0IGHQJNHUEHXEAAKA1VTBA5WPXEZD3MRKNXP0ZRRK'
-      $.ajax({
-        url: urlString,
-        method: "GET",
-      success: function(data){
-        var element = new placeItemModel({},'','','','',id, false, true)
-        element.name = data.response.venue.name
-        element.description = data.response.venue.description
-        element.phone = data.response.venue.contact.formattedPhone
-        element.address = data.response.venue.location.address
-        element.marker = new google.maps.Marker({
+  for (let idx in listVenueIds) {
+    let id = listVenueIds[idx]
+    let urlString = 'https://api.foursquare.com/v2/venues/'+id+
+    '?v=20171010&client_id='+
+    'SEHUEOSE3XRMJMKEK5SZVIQE3DKILVAKUJMAMQQAUWQSHWSY&client_'+
+    'secret=21PN43B0IGHQJNHUEHXEAAKA1VTBA5WPXEZD3MRKNXP0ZRRK'
+    $.ajax({
+      url: urlString,
+      method: "GET",
+  async: false,
+    success: function(data){
+      var element = new placeItemModel('','','','',id, false, true)
+      element.name = data.response.venue.name
+      element.description = data.response.venue.description
+      element.phone = data.response.venue.contact.formattedPhone
+      element.address = data.response.venue.location.address
+      listMarkers[idx] = new google.maps.Marker({
         position: {lat: data.response.venue.location.lat,
-          lng: data.response.venue.location.lng},
+        lng: data.response.venue.location.lng},
         map: map,
         animation: google.maps.Animation.DROP,
         title: data.response.venue.name})
-        element.marker.addListener('click',function(){toggleActive(element)})
-        listPlaces.push(element)
-        vm.listPlaces().push(element)
-        if(listVenueIds.length == i+1)
-          ko.applyBindings(vm)
-        i=i+1
-      },
-      error: function(data){
-        console.log("error retrieving data ")
-      }})
-    }
- }
-var ViewModel = function () {
-  let self = this
-  self.listPlaces = ko.observableArray([])
+      listMarkers[idx].addListener('click',function(){toggleActive(element,listMarkers)})
+      listPlaces[idx]=element
+      vm.listPlaces()[idx]=element
+    },
+    error: function(data){
+      console.log("error retrieving data ")
+    }})
+  }
+  ko.applyBindings(vm)
+
   function setMapOnAll(map) {
-        for (var i = 0; i < self.listPlaces().length; i++) {
-          self.listPlaces()[i].marker.setMap(map);
+        for (var i = 0; i < listMarkers.length; i++) {
+          listMarkers[i].setMap(map);
         }
       }
-  self.mySearch = function(){
-  console.log("my search "+document.getElementById("mySearch").value)
-  var query = document.getElementById("mySearch").value
-  //listPlaces match on title
-  //set isVisible = true
-    setMapOnAll(null)
-  console.log(self.listPlaces())
-  for (var place of self.listPlaces()) {
-    if(place.marker.title.toLowerCase().indexOf(query.toLowerCase()) !== -1){
-      place.isVisible(true)
-      place.marker.setMap(map)
-    }
-    else {
-      place.isVisible(false)
-    }
-  }
-}
-  self.myCancel = function(){
-        setMapOnAll(map)
-        for (var place of self.listPlaces()) {
-            place.isVisible(true)
-        }
-    }
-  self.onClick = function (place){
-    console.log("on click "+place.marker.title)
-    for (var placem of self.listPlaces()) {
-        placem.isActive=false
-    }
-      toggleActive(place)
-  }
-}
+ }
